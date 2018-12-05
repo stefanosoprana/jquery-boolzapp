bf9165cbaa61709d502ef42e799b74c67d52396c
stefanosoprana <stefano.soprana@icloud.com>
2018-12-05 13:29:22 +0100

    filter contacts

--- main.js
+++ main.js
@@ -13,19 +13,35 @@ $(document).ready(function(){
       $('.main-up').append(newmessageTemplate);
       $('.main-up').append(clearTemplate);
 
-      setTimeout(countDown, 1000);
-      function countDown() {
+      setTimeout(function() {
 
         var newmessageTemplateBot = $('.template .message.received').clone();
-        var clearTemplateBot = $('.template .clearBot').clone();
+        var clearTemplateBot = $('.template .clear').clone();
         newmessageTemplateBot.text('ok');
         $('.main-up').append(newmessageTemplateBot);
         $('.main-up').append(clearTemplateBot);
 
-      }
+      }, 1000);
 
       $('#newMessage').val('');
 
     }
   });
+
+    $('#newContact').keyup(function(){
+      var thisValue = $(this).val().toLowerCase();
+  		console.log(thisValue);
+
+      $('.contacts li').each(function() {
+      var contactName = $(this).find('.contact-name').text().toLowerCase();
+      console.log(contactName);
+        if (contactName.includes(thisValue)) {
+          $(this).show();
+        }
+        else {
+          $(this).hide();
+        }
+      });
+    });
+
 });
