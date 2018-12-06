9b4b9da2be238ea83d8914e2199284b5478af940
stefanosoprana <stefano.soprana@icloud.com>
2018-12-06 11:23:35 +0100

    Time contacts

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
 $(document).ready(function(){
 
+  var now = new Date();
+
+  var messageReceivedTimeSet = $('.message .message-time')
+  messageReceivedTimeSet.text(now.getHours() + ':' + now.getMinutes());
+
   $('#newMessage').keypress(function(event){
 
     if (event.which == 13) {
 
       var newMessageValue = $('#newMessage').val();
       var newmessageTemplate = $('.template .message.sent').clone();
-      var clearTemplate = $('.template .clear').clone();
-
-      newmessageTemplate.text(newMessageValue);
+      newmessageTemplate.children('.message-text').text(newMessageValue);
+      newmessageTemplate.children('.message-time').text(now.getHours() + ':' + now.getMinutes());
 
       $('.main-up').append(newmessageTemplate);
+
+
+      var clearTemplate = $('.template .clear').clone();
       $('.main-up').append(clearTemplate);
 
       setTimeout(function() {
 
         var newmessageTemplateBot = $('.template .message.received').clone();
-        var clearTemplateBot = $('.template .clear').clone();
-        newmessageTemplateBot.text('ok');
+        newmessageTemplateBot.children('.message-text').text('Ok');
+        newmessageTemplateBot.children('.message-time').text(now.getHours() + ':' + now.getMinutes());
         $('.main-up').append(newmessageTemplateBot);
+
+
+        var clearTemplateBot = $('.template .clear').clone();
         $('.main-up').append(clearTemplateBot);
 
       }, 1000);
